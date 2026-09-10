26a7fdd4d2bc5d0e88f4fdfdd087cefff45ad87a
LOKESHKALE1 <144276094+LOKESHKALE1@users.noreply.github.com>
2026-09-10 15:26:49 +0530

    compelted

--- sample.py
+++ sample.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from supabase import create_client
 from dotenv import load_dotenv
 import os
@@ -7,14 +6,6 @@ import os
 load_dotenv()
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
